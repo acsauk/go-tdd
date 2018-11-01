62c6e199df28c3f3864b935c5f685fcc21d4b636
acsauk <acsauk@gmail.com>
2018-11-01 11:37:38 +0000

    refactors to extract insufficient funds error to package var, uses new var in tests

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -13,10 +13,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t *testing.T, err error) {
+	assertError := func(t *testing.T, got error, want error) {
 		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didnt get one")
+		if got == nil {
+			t.Fatal("wanted an error but didnt get one")
+		}
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
 		}
 	}
 
@@ -38,7 +42,7 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err)
+		assertError(t, err, ErrInsufficientFunds)
 	})
 	
 }
