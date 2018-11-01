ff4e9e7fb32bdb737952dab82a44f3a92a175359
acsauk <acsauk@gmail.com>
2018-11-01 12:19:06 +0000

    refactors to extract magix string to ErrNotFound, adds helper assertError and replaces assertString in error test with assertError

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,16 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		assertError(t, got, ErrNotFound)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -15,5 +22,13 @@ func assertStrings(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error '%s', want '%s'", got, want)
 	}
 }
